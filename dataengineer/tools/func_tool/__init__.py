f9075288df627ad855f1f2090647953135032d7e
linnanjun007-crypto <linnanjun007@gmail.com>
2026-08-05 09:39:55 +0800

    新增函数式工具（数据库/文件/询问）

---
+++ dataengineer/tools/func_tool/__init__.py
@@ -0,0 +1,43 @@
+# Copyright 2025-present DatusAI, Inc.
+# Licensed under the Apache License, Version 2.0.
+# See http://www.apache.org/licenses/LICENSE-2.0 for details.
+
+from dataengineer.tools.func_tool.ask_user_tools import AskUserTool
+from dataengineer.tools.func_tool.base import FuncToolResult, trans_to_function_tool
+from dataengineer.tools.func_tool.context_search import ContextSearchTools
+from dataengineer.tools.func_tool.database import DBFuncTool, db_function_tool_instance, db_function_tools
+from dataengineer.tools.func_tool.date_parsing_tools import DateParsingTools
+from dataengineer.tools.func_tool.filesystem_tools import FilesystemFuncTool, filesystem_function_tools
+from dataengineer.tools.func_tool.gen_semantic_model_tools import GenSemanticModelTools
+from dataengineer.tools.func_tool.generation_tools import GenerationTools
+from dataengineer.tools.func_tool.plan_tools import PlanTool, SessionTodoStorage
+from dataengineer.tools.func_tool.platform_doc_search import PlatformDocSearchTool
+from dataengineer.tools.func_tool.semantic_tools import SemanticTools
+from dataengineer.tools.func_tool.sub_agent_task_tool import SubAgentTaskTool
+
+__all__ = [
+    "trans_to_function_tool",
+    "FuncToolResult",
+    "AskUserTool",
+    "DBFuncTool",
+    "db_function_tools",
+    "db_function_tool_instance",
+    "ContextSearchTools",
+    "DateParsingTools",
+    "GenerationTools",
+    "PlanTool",
+    "SessionTodoStorage",
+    "filesystem_function_tools",
+    "FilesystemFuncTool",
+    "SemanticTools",
+    "GenSemanticModelTools",
+    "PlatformDocSearchTool",
+    "SubAgentTaskTool",
+]
+
+try:
+    from dataengineer.tools.func_tool.scheduler_tools import SchedulerTools  # noqa: F401
+
+    __all__.append("SchedulerTools")
+except ImportError:
+    pass
